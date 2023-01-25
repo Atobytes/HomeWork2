497d8119c3f174bbf4afd4740e30eb0b0c0a6f9f
Atobytes <107487580+Atobytes@users.noreply.github.com>
2023-01-25 15:24:38 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         int ticket = 1000;
         int bonusForTicket = 20;
-        System.out.println((ticket / bonusforticket) + " Полученные бонуснуые Мили");
+        System.out.println((ticket / bonusForTicket) + " Полученные бонуснуые Мили");
     }
 }
